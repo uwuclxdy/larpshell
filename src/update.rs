3477b951e8174c39ccc5d7698895b32c200b513b
cloudy <37777261+uwuclxdy@users.noreply.github.com>
2026-04-03 21:51:36 +0200

    feat: show update instructions based on installation method

--- src/update.rs
+++ src/update.rs
@@ -19,6 +19,35 @@ struct CrateData {
     newest_version: String,
 }
 
+enum InstallMethod {
+    Cargo,
+    AUR,
+    Unknown,
+}
+
+fn detect_install_method() -> InstallMethod {
+    let Ok(exe) = std::env::current_exe() else {
+        return InstallMethod::Unknown;
+    };
+    let path = exe.to_string_lossy();
+
+    if path.starts_with("/usr") {
+        InstallMethod::AUR
+    } else if path.contains(".cargo/bin") {
+        InstallMethod::Cargo
+    } else {
+        InstallMethod::Unknown
+    }
+}
+
+fn update_instruction() -> &'static str {
+    match detect_install_method() {
+        InstallMethod::Cargo => ", run `cargo install larpshell`",
+        InstallMethod::AUR => ", run `yay -S larpshell` or `yay -S larpshell-git`",
+        InstallMethod::Unknown => "",
+    }
+}
+
 pub async fn is_update_available() -> bool {
     let Ok(client) = reqwest::Client::builder()
         .user_agent(concat!("larpshell/", env!("CARGO_PKG_VERSION")))
@@ -54,7 +83,7 @@ pub fn print_if_resolved() {
 fn print_notice() {
     eprintln!(
         "{}",
-        "update available, run `cargo install-update larpshell`"
+        format!("update available{}", update_instruction())
             .custom_color(CTP_PRIMARY)
             .bold()
     );
